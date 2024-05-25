69cc65b8a306f787af7dd10a9476649edfeec9f1
Pradum <satendarpradum@gmail.com>
2024-05-25 10:58:58 +0530

    Added A Login Endpoint It's More Like A BoilerPlate

--- Self-Passport-Practice-2/routes/index.js
+++ Self-Passport-Practice-2/routes/index.js
@@ -10,6 +10,32 @@ router.post('/signUp',passport.authenticate('signUp',{session:false}),async(req,
     })
 })
 
+router.post('/signin',passport.authenticate('login',async(req,res,next)=>{
+    passport.authenticate('login',async(err,user,info)=>{
+        try {
+               if (err||!user) {
+                const error=new Error('An Error Occured');
+                return next(error);
+               }
+
+               req.login(user,{session:false},async(error)=>{
+                if (error) {
+                    return next(error);
+                }
+                const body={_id:user._id,email:user.email};
+                const token=jwt.sign({user:body},'TOP_SECRET');
+                return res.json({token});
+               })
+
+
+
+
+        } catch (error) {
+            return next(error);
+        }
+    })(req,res,next);
+}))
+
 
 
 module.exports=router;
